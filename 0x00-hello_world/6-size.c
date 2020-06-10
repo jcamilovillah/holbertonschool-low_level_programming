c56910d5754c77410c07cc3e4092bf850964cf80
jcamilovillah <villahjuancamilo@gmail.com>
2020-06-10 22:51:20 +0000

    C if sun

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Function to show the bites of each data type
+* Return: 0
+**/
+
+int main(void)
+{
+
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(c));
+	printf("Size of an int: %zu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(lli));
+	printf("Size of a float: %zu byte(s)\n", sizeof(f));
+	return (0);
+
+}
