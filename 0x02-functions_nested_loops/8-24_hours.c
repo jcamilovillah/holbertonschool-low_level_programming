ec671bf3892b7c6f14913d431efbe22aed32e0b6
jcamilovillah <villahjuancamilo@gmail.com>
2020-06-16 20:17:45 +0000

    C-loop

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * jack_bauer - Print the hours
+ * Return: nothing because is a void
+ **/
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
