b56fb71748a234d38bb64dbf7ffa92f7851fa63d
jcamilovillah <villahjuancamilo@gmail.com>
2020-06-23 16:59:40 +0000

    Pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/***
+ *swap_int - swaps two integers.
+ *@a: argument used as a punter.
+ *@b: argument used as a punter.
+ */
+
+void swap_int(int *a, int *b)
+{
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
