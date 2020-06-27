37e537bd4ace9e8fc07dcb86de6cbd5a7446e666
jcamilovillah <villahjuancamilo@gmail.com>
2020-06-27 05:05:10 +0000

    More pointers

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,27 +1,27 @@
 #include "holberton.h"
 
 /**
- * rot13 - Function that reverses the content of an array of integers.
- *
- * @str: s is the array
- *
- * Return: Always 0.
- */
-char *rot13(char *str)
-{
-	int i = 0, k;
-	char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+* rot13 - function that encodes a string using rot13
+* @a: string
+* Return: string changed
+**/
 
-	for (; str[i] != '\0'; i++)
+char *rot13(char *a)
+{
+	int i, j;
+	char rot[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char tor[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; a[i] != '\0'; i++)
 	{
-		for (k = 0; k <= 51; k++)
+		for (j = 0; j <= 51; j++)
 		{
-			if (s[k] == str[i])
+			if (rot[j] == a[i])
 			{
-				str[i] = s1[k];
+				a[i] = tor[j];
+				break;
 			}
 		}
 	}
-	return (str);
+	return (a);
 }
