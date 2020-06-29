bf680a487d57e856951668a8e5d206f8cc6d2aec
jcamilovillah <villahjuancamilo@gmail.com>
2020-06-29 22:11:08 +0000

    Double pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include <holberton.h>
+
+/* *_memset - name of the function.
+ * @s:  pointer
+ *@b: string
+ *@n: number the bytes
+ * Return: Always 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i=0 ; i<n ; i++){
+		s[i]= b;
+	}
+}
