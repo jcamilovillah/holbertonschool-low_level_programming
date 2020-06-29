25a61f4196b531c28b998b44c95e281713d63587
jcamilovillah <villahjuancamilo@gmail.com>
2020-06-29 23:54:04 +0000

    Double pointers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _memcpy; name function
+ * @dest: pointer memory area
+ * @src: pointer memory area
+ * @n: bytes
+ * Return : pointer dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest)
+}
