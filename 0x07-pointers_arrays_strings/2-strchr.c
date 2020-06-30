3895af23bb49da94f826a67343721e92294b6ca7
jcamilovillah <villahjuancamilo@gmail.com>
2020-06-30 02:15:18 +0000

    Double pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Entry point
+ *@s: pointer
+ *@c: string
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0' ; s++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	if (s[i] == c)
+	{
+		return (s + i);
+	}
+	return (0);
+}
