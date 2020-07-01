75cb10b3017107a3a564e5e082ff6088b82f0e2d
jcamilovillah <villahjuancamilo@gmail.com>
2020-07-01 03:41:22 +0000

    Double pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,18 @@
 #include "holberton.h"
-
 /**
- * print_chessboard - Entry point
- * Desc: Entry
- *@a: pointer
- * Return: Always 0 (Success)
+ * print_chessboard - prints the chessboard.
+ * @a: array to print
+ * Return: Nothing.
  */
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0 ; i < 8 ; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = 0 ; j < 8 ; j++)
+		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j]);
+			_putchar(*(*(a + i) + j));
 		}
 		_putchar('\n');
 	}
