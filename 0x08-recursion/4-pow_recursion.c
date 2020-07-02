f88498530186546420016aa4fe5b57609482889b
jcamilovillah <villahjuancamilo@gmail.com>
2020-07-02 04:25:07 +0000

    Recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* _pow_recursion - Function.
+* @x: number
+* @y: variable
+* Return: Always 0.
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, (y - 1)));
+}
