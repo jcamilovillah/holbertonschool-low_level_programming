9365c71810cf53523fa857eed8bd13bd33b45754
jcamilovillah <villahjuancamilo@gmail.com>
2020-07-02 04:55:13 +0000

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - Entry point
+ * Desc: Entry
+ *@s: pointer
+ * Return: Always 0 (Success)
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (*s);
+}
