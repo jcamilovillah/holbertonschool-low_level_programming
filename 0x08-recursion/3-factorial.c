c5a1423fed17b359331517085440a34ed7925be9
jcamilovillah <villahjuancamilo@gmail.com>
2020-07-02 03:46:32 +0000

    Recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 
 /**
- * factorial - Entry point
- *@s: pointer
+ *factorial - Entry point
+ *@n: pointer
  * Return: Always 0 (Success)
  */
 
 int factorial(int n)
 {
-
 	if (n < 0)
 	{
 		return (-1);
