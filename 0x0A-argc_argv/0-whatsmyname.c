3c6177a9b6dadbbeac0d7da77a172bc68a48901e
jcamilovillah <villahjuancamilo@gmail.com>
2020-07-06 00:43:03 +0000

    Main arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print name.
+* @argc: int.
+* @argv: char.
+* Return: Always 0.
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
