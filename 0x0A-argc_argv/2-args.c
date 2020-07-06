b2c70a250f7ebada4c0f1249ac0f919eb4ee2bc5
jcamilovillah <villahjuancamilo@gmail.com>
2020-07-06 02:34:18 +0000

    Main arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - print arguments
+* @argc: int
+* @argv: char
+* Return: Always 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
