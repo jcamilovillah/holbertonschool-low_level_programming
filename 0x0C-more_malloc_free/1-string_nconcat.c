200036cda22c5095db814449dbc6f6fa00bdede4
jcamilovillah <villahjuancamilo@gmail.com>
2020-07-10 03:19:00 +0000

    More malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,53 +1,45 @@
 #include "holberton.h"
 
 /**
-* string_nconcat - concatenates two strings.
-* @s1: 1st string
-* @s2: 2nd string
-* @n: limit
-* Return: Always 0.
-*/
+ * string_nconcat - concatenate two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: limit
+ *
+ * Return: pointer
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *t;
-	unsigned int x, z;
+	/* unsigned int var because we need to compare with n */
+	char *p;
+	unsigned int i, j;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
 	}
 
-	for (x = 0; s1[x] != '\0'; x++)
-	{
+	if (n > j)
+		n = j;
 
-	}
-	for (z = 0; s2[z] != '\0'; z++)
-	{
-
-	}
-	if (n >= z)
-	{
-		n = z;
-	}
-	t = malloc((x + n + 1) * sizeof(char));
-
-	if (t == NULL)
-	{
+	p = malloc ((i + n + 1) * sizeof(char));
+	if (p == NULL)
 		return (NULL);
-	}
 
-	for (x = 0; s1[x] != '\0'; x++)
-	{
-		t[x] = s1[x];
-	}
-	for (z = 0; z < n; z++, x++)
-	{
-		t[x] = s2[z];
-	}
-	t[x] = '\0';
-	return (t);
+	for (i = 0; s1[i] != '\0'; i++)
+		strings[i] = s1[i];
+
+	for (j = 0; j < n; j++, i++)
+	p[i] = s2[j];
+	p[i] = '\0';
+	return (p);
 }
