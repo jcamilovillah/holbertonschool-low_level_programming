81a36ebd11800230870053ff4379433a5eb67da6
jcamilovillah <villahjuancamilo@gmail.com>
2020-07-14 01:29:14 +0000

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
