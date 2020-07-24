9541b7392ffb5391a4f7cc18e692ce6692789b7e
jcamilovillah <villahjuancamilo@gmail.com>
2020-07-23 21:20:17 -0500

    singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: nodes.
+ * Return: elements.
+ */
+size_t print_list(const list_t *h)
+{
+	int i;
+	const list_t *nodo = h;
+
+	for (i = 0; nodo != NULL; i++)
+	{
+		if (nodo->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", nodo->len, nodo->str);
+		}
+		nodo = nodo->next;
+	}
+	return (i);
+}
