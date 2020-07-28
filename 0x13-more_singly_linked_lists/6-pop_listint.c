a40d594e631d828468efdf531a7bc0d1bced1efd
jcamilovillah <villahjuancamilo@gmail.com>
2020-07-28 17:24:44 -0500

    delete head

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * free_listint2 - function that frees a listint_t list.
+ * pop_listint - delete the head node.
  * @head: first element
  * Return: void.
  */
@@ -9,6 +9,7 @@ int pop_listint(listint_t **head)
 	if (*head != NULL)
 	{
 		listint_t *aux_delete;
+
 		aux_delete = *head;
 		int f = 0;
 
