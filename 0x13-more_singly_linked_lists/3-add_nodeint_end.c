c115bea3460940ab5b158c7639bd33a45291cd3a
jcamilovillah <villahjuancamilo@gmail.com>
2020-07-28 12:08:48 -0500

    add end list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -22,12 +22,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = new;
-        return (new);
-    }
-		while (aux->next != NULL)
-		{
-			aux = aux->next;
-	    }
-        aux->next = new;
-return (new);
+		return (new);
+	}
+	while (aux->next != NULL)
+	{
+		aux = aux->next;
+	}
+	aux->next = new;
+	return (new);
 }
