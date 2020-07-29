41c3de7034b421bd2656f052c5a304d203b01d0c
jcamilovillah <villahjuancamilo@gmail.com>
2020-07-28 22:31:44 -0500

    head is't pointer

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index - adds a new node at the beginning of a listint_t list.
+ * get_nodeint_at_index - that returns the nth node of a listint_t linked list.
  * @index: index
  * @head: first node
  * Return: nth node.
@@ -8,19 +8,20 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *nodo = head;
+	listint_t *nodo;
 
 	if (head)
 	{
 		for (i = 0; i < index; i++)
 		{
-			if (head == NULL)
+			nodo = head;
+			if (nodo == NULL)
 			{
 				return (NULL);
 			}
-			head = nodo->next;
+			nodo = nodo->next;
 		}
-		return (head);
+		return (nodo);
 	}
 	return (NULL);
 }
