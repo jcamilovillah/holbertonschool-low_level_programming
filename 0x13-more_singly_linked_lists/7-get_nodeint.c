db957c42cde6d87aae5a3fb3e12a9b830f92cba4
jcamilovillah <villahjuancamilo@gmail.com>
2020-07-28 22:46:48 -0500

    return nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head != NULL)
 	{
-        nodo = head;
+		nodo = head;
 		for (i = 0; i < index; i++)
 		{
 			if (nodo == NULL)
