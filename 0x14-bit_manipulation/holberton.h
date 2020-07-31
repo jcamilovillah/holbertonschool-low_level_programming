bf5541d569de6a3519c11b22a916a6f152cb1db5
jcamilovillah <villahjuancamilo@gmail.com>
2020-07-30 19:08:11 -0500

    print binary

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,9 +2,11 @@
 #define HOLBERTON_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+int _putchar(char c);
 void print_binary(unsigned long int n);
-void binary_print(unsigned long int n)
+void binary_print(unsigned long int n);
 
 
 #endif
