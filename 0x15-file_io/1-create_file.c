31c96b163b85cc9b852a8fbd5390fb44fc8735ae
jcamilovillah <villahjuancamilo@gmail.com>
2020-08-04 20:57:36 -0500

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 		if (fd < 0)
-			return (0);
+			return (-1);
 
 		if (text_content)
 		{
