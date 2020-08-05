a126c897f062fa71abcd0d9b7aca0006297fb5c1
jcamilovillah <villahjuancamilo@gmail.com>
2020-08-04 20:53:48 -0500

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * create_file - function that creates a file.
+ * @filename: pointer to file.
+ * @text_content: string.
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i;
+
+	if (filename)
+	{
+		fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		if (fd < 0)
+			return (0);
+
+		if (text_content)
+		{
+			for (i = 0; text_content[i] != '\0'; i++)
+			{
+			}
+			write(fd, text_content, i);
+		}
+		close(fd);
+		return (1);
+	}
+	return (-1);
+}
