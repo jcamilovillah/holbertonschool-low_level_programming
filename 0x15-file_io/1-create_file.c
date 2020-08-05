ed366813836eb3d9399ebe805f3457d4e8b02572
jcamilovillah <villahjuancamilo@gmail.com>
2020-08-04 21:41:21 -0500

    change format

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
 		if (fd < 0)
 			return (-1);
 
