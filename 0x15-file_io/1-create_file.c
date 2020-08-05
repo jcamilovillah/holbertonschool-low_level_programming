34d98358d94c03d3b6f820e942b3eaffceb4c005
jcamilovillah <villahjuancamilo@gmail.com>
2020-08-04 21:21:28 -0500

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - function that creates a file.
  * @filename: pointer to file.
  * @text_content: string.
- * Return: 1 or -1
+ * Return: 1 or -1.
  */
 int create_file(const char *filename, char *text_content)
 {
