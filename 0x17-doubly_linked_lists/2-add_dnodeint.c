cab0f6b27502f0ce73ec3034913a92938a913471
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-06 16:42:08 -0500

    validation

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,17 +9,22 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	if (head == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
 	new->n = n;
-	/*new->prev = NULL;*/
-	new->next = *head;
+	new->prev = NULL;
+	if (!(*head))
+	{
+		new->next = NULL;
+	}
+	else
+	{
+		new->next = *head;
+		(*head)->prev = new;
+	}
 	*head = new;
 	return (new);
 }
