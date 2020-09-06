ec01497669c7c24ebe7670947cacae9ba9d06fa6
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-06 16:12:55 -0500

    validation

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	new->n = n;
-	//new->prev = NULL;
+	/*new->prev = NULL;*/
 	new->next = *head;
 	*head = new;
 	return (new);
