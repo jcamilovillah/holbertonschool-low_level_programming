900206d10574ecf013c1da1acc7e9c418bff05d2
jcamilovillah <villahjuancamilo@gmail.com>
2020-10-02 18:00:05 -0500

    header

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,5 +40,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 hash_node_t *add_nodeint(hash_node_t **h, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 #endif
