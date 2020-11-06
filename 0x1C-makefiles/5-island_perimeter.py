5f6ea355d1b482dcee27a3ae295104b91490b60f
jcamilovillah <villahjuancamilo@gmail.com>
2020-11-06 16:49:09 -0500

    easy task

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module of function"""
+
+
+def island_perimeter(grid):
+    """that returns the perimeter of the island described in grid"""
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1 and p == 0:
+                p += 4
+            if grid[i][j] == 1:
+                p += 2
+    return p - 2
